4e1ebcfa1fd2e7d541314ab4dd87920aca1e888d
Fumihiro <fumihiro@exa.phys.s.u-tokyo.ac.jp>
2019-11-11 12:15:25 +0900

    modify bug

--- include/physics/harmonic_oscillator_fixed_end.hpp
+++ include/physics/harmonic_oscillator_fixed_end.hpp
@@ -9,15 +9,15 @@ namespace hamiltonian{
 
 class HarmonicOscillatorFixedEnd  {
 public:
-  static std::string name() { return "Classical Harmonic Oscillator : Fixed End"; }
+  static std::string name() { return "Classical Harmonic Oscillator : Fixed End : must use Open Boundary Chain Lattice"; }
   // z[0...n-1]: position, z[n...2n-1]: momentum
-  HarmonicOscillatorFixedEnd(unsigned int num_particles, double J, std::vector<std::vector<int > > table, unsigned int N_adj) : n_(num_particles), J_(J), table_(table),Nd_(N_adj){}
+  HarmonicOscillatorFixedEnd(int num_particles, double J, std::vector<std::vector<int > > table, int N_adj) : n_(num_particles), J_(J), table_(table),Nd_(N_adj){}
   double energy(double t, std::vector<double> const& z) const {
     return potential_energy(t, z) + kinetic_energy(t, z);
   }
   double potential_energy(double  t, std::vector<double> const& z) const {
     double ene = 0;
-    for(int i = 0; i < n_ ; ++i) ene += target_potential_energy(i,z,t);
+    for(int i = -1; i < n_ ; ++i) ene += target_potential_energy(i,z,t);
     return ene/2.0;
   }
   double kinetic_energy(double /* t */, std::vector<double> const& z) const {
@@ -26,17 +26,21 @@ public:
     return ene;
   }
 
-  double target_potential_energy(unsigned int l, std::vector<double> const& z, double t) const {
+  double target_potential_energy(int l, std::vector<double> const& z, double t) const {
     const double *x = &z[0];
     double ene = 0;
-    for(int i = 0; i < Nd_ ; ++i){
-      double dx = x[l] - x[table_[l][i]];
-      ene += 0.5* J_ * dx * dx;
+    if( l != -1){
+      if(l == 0 || l == n_ - 1) ene += 0.5 * J_ * x[l] * x[l];
+      for(int i = 0; i < Nd_ ; ++i){
+        double dx = x[l] - x[table_[l][i]];
+        ene += 0.5* J_ * dx * dx;
+      }
     }
+    else for(auto& i : {0, n_ -1}) ene += 0.5 * J_ * x[i] * x[i];
     return ene;
   }
 
-  double target_kinetic_energy(unsigned int l, std::vector<double> const& z) const {
+  double target_kinetic_energy(int l, std::vector<double> const& z) const {
     const double *v = &z[n_];
     double ene = 0;
     ene += 0.5 * v[l]*v[l];
@@ -49,25 +53,24 @@ public:
     const double *v = &z[n_];
     double *fx = &force[0];
     double *fv = &force[n_];
-    fx[0] = 0.0;
-    fv[0] = 0.0;
-    for(int i = 1; i < n_ ; ++i) fx[i] = v[i];
-    for(int i = 1; i < n_ ; ++i){
+    for(int i = 0; i < n_ ; ++i) fx[i] = v[i];
+    for(int i = 0; i < n_ ; ++i){
       fv[i] = 0.0;
       for(int d = 0; d < Nd_; ++d){
-        fv[i] += -1.0 * J_ *(x[i] -x[table_[i][d]]);
+        fv[i] -=  J_ * (x[i] -x[table_[i][d]]);
       }
     }
+    for(auto& i : {0, n_ - 1}) fv[i] -= J_ * x[i];
   }
 
-  unsigned int Nd()const { return Nd_ ;}
+  int Nd()const { return Nd_ ;}
   int table(int i, int j)const{ return table_[i][j];} 
 
 private:
-  unsigned int n_;
+  int n_;
   double J_;
   std::vector<std::vector<int> > table_;
-  unsigned int Nd_;
+  int Nd_;
 };
 
 
