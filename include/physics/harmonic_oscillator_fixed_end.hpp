09ff7da47bde1f29a23825a07c79c93b606cd47d
Fumihiro <fumihiro@exa.phys.s.u-tokyo.ac.jp>
2019-11-12 17:28:52 +0900

    modify bug

--- include/physics/harmonic_oscillator_fixed_end.hpp
+++ include/physics/harmonic_oscillator_fixed_end.hpp
@@ -17,33 +17,19 @@ public:
   }
   double potential_energy(double  t, std::vector<double> const& z) const {
     double ene = 0;
-    for(int i = -1; i < n_ ; ++i) ene += target_potential_energy(i,z,t);
-    return ene/2.0;
+    const double *x = &z[0.0];
+    for(int i = 0; i < n_ -1; ++i){
+      double dx = x[i] - x[table_[i][1]];
+      ene += 0.5* J_ * dx * dx;
+    }
+    ene += 0.5 * J_ * x[0] * x[0];
+    ene += 0.5 * J_ * x[n_-1] * x[n_-1];
+    return ene;
   }
   double kinetic_energy(double /* t */, std::vector<double> const& z) const {
     double ene = 0;
-    for(int i = 0; i < n_; ++i) ene += target_kinetic_energy(i,z);
-    return ene;
-  }
-
-  double target_potential_energy(int l, std::vector<double> const& z, double t) const {
-    const double *x = &z[0];
-    double ene = 0;
-    if( l != -1){
-      if(l == 0 || l == n_ - 1) ene += 0.5 * J_ * x[l] * x[l];
-      for(int i = 0; i < Nd_ ; ++i){
-        double dx = x[l] - x[table_[l][i]];
-        ene += 0.5* J_ * dx * dx;
-      }
-    }
-    else for(auto& i : {0, n_ -1}) ene += 0.5 * J_ * x[i] * x[i];
-    return ene;
-  }
-
-  double target_kinetic_energy(int l, std::vector<double> const& z) const {
     const double *v = &z[n_];
-    double ene = 0;
-    ene += 0.5 * v[l]*v[l];
+    for(int i = 0; i < n_; ++i) ene += 0.5 * v[i]*v[i];
     return ene;
   }
 
@@ -54,13 +40,16 @@ public:
     double *fx = &force[0];
     double *fv = &force[n_];
     for(int i = 0; i < n_ ; ++i) fx[i] = v[i];
-    for(int i = 0; i < n_ ; ++i){
+    for(int i = 1; i < n_ -1; ++i){
       fv[i] = 0.0;
       for(int d = 0; d < Nd_; ++d){
         fv[i] -=  J_ * (x[i] -x[table_[i][d]]);
       }
     }
-    for(auto& i : {0, n_ - 1}) fv[i] -= J_ * x[i];
+  fv[0] = 0.0;
+  fv[0] -= J_ * (x[0] - x[1]) + J_ * x[0];
+  fv[n_-1] = 0.0;
+  fv[n_-1] -= J_ * (x[n_-1] - x[n_-2]) + J_ * x[n_-1];
   }
 
   int Nd()const { return Nd_ ;}
